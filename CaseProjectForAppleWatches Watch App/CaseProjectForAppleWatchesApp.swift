39de165ebcba43a70e9a9c3138b15a34036185e3
Ataberk Kılınçtekin <94781211+AtaberkKilinctekin@users.noreply.github.com>
2022-10-22 08:23:46 +0300

    Initial Commit

---
+++ CaseProjectForAppleWatches Watch App/CaseProjectForAppleWatchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseProjectForAppleWatchesApp.swift
+//  CaseProjectForAppleWatches Watch App
+//
+//  Created by Ataberk on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CaseProjectForAppleWatches_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaseProjectForAppleWatches/CaseProjectForAppleWatchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseProjectForAppleWatchesApp.swift
+//  CaseProjectForAppleWatches
+//
+//  Created by Ataberk on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CaseProjectForAppleWatchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
